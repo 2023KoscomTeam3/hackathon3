cf42918aebe23387112e5cf300a48467711814dc
LEE HYE JIN <108391677+Hiluxy@users.noreply.github.com>
2023-08-14 09:33:53 +0900

    fix method

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,16 +1,8 @@
 <template>
     <div id="header">
-<<<<<<< HEAD
-        <h1>KosLetter</h1>
+        <img id="titleLogo" @click='goHome'  src="../../assets/kosletter.png"/>
         <div id="mypage">
-            <a href="http://localhost:8080/about">
-                <h2>{{username}}1111</h2>
-            </a>
-=======
-        <img id='titleLogo' @click='goHome' src="../../assets/kosletter.png" />
-        <div id="myInfo">
-            <h2 @click='goAbout'>닉네임</h2>
->>>>>>> backup
+            <h2 @click='goAbout'> {{ username }}1111</h2>
             <h3>220코인</h3>
             <b-button>코인 사용하기</b-button>
         </div>
@@ -20,19 +12,7 @@
 <script>
 export default {
     name : "header",
-<<<<<<< HEAD
     props:['username'],//데이터받기
-};
-</script>
-
-<style>
-@font-face {
-    font-family: 'ChosunBg';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/ChosunBg.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-=======
     data()  {
         return {
             isHome : true
@@ -57,8 +37,13 @@ export default {
 };
 </script>
 
-<style scoped>
->>>>>>> backup
+<style>
+@font-face {
+    font-family: 'ChosunBg';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/ChosunBg.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 #header {
     height : 150px;
     background-color : #e5e3e1;
@@ -66,7 +51,8 @@ export default {
     justify-content: flex-start;
     padding : 15px;
 }
-#myInfo {
+#mypage {
     margin-left : 700px;
 }
+
 </style>
